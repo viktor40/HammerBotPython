969ac3baf55b4674b10f0261fcab124b28b3fb68
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-07-06 00:45:27 +0200

    added a description option to the multiple_vote command

--- voting.py
+++ voting.py
@@ -1,8 +1,19 @@
-def convert_multiple_vote(arg):
+# HammerBotPython
+# voting.py
+
+from data import discord_letters
+
+
+def convert_multiple_vote(args):
     poll_list = []
+    introduction = ""
     vote = ""
     for i in args:
-        if "&" not in i:
+        if "|" in i:
+            vote += i[:-1]
+            introduction = vote
+            vote = ""
+        elif "&" not in i:
             vote += i + " "
         else:
             vote += i[:-1]
@@ -11,6 +22,7 @@ def convert_multiple_vote(arg):
 
     poll_list.append(vote)
     poll = ""
+    print(introduction)
     for pos, option in enumerate(poll_list):
         poll += discord_letters[pos] + " " + option + "\n"
-    return poll, poll_list
+    return poll, poll_list, introduction
