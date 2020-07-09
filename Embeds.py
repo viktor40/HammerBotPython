d2771f38cf4e4ca170d921b5ccb509e14958cacf
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-07-09 16:40:03 +0200

    added __add__ to TaskEmbed

--- Embeds.py
+++ Embeds.py
@@ -51,7 +51,8 @@ class RichEmbed:
 class TaskEmbed(RichEmbed):
     def __init__(self, project, formatted, channel_history, color="", title="", description="", url="", author=""
                  , author_icon="", footer="", fields=None):
-        super(TaskEmbed, self).__init__(self, color, title, description, url, author, author_icon, footer, fields)
+        super(TaskEmbed, self).__init__(self, color=color, title=title, description=description, url=url,
+                                        author=author, author_icon=author_icon, footer=footer, fields=fields)
         self.project = project
         self.formatted = formatted
         self.channel_history = channel_history
@@ -65,4 +66,14 @@ class TaskEmbed(RichEmbed):
         return embed
 
     def __add__(self, other):
-        pass
+        if self.project[:-1] == " ":
+            project = self.project[:-1]
+        for message in self.channel_history:
+            if message.embeds:
+                if message.embeds[0].title == project:
+                    edited_embed = discord.Embed(
+                        color=0xe74c3c,
+                        title=message.embeds[0].title,
+                        description=message.embeds[0].description + "\n" + formatted
+                    )
+                    return message, edited_embed
