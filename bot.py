958a2c24cfcc2c6d2eb634341cb013ce7aca6bea
Grohiik <43449017+Grohiik@users.noreply.github.com>
2020-07-14 15:50:25 +0200

    Update bot.py
    so it only checks the first 3 bugs

--- bot.py
+++ bot.py
@@ -1,248 +1,110 @@
 # HammerBotPython
-# bot.py
+# bug.py
 
 import discord
-from discord.ext import commands
+import re
+from jira import JIRA
 from dotenv import load_dotenv  # load module for usage of a .env file (pip install python-dotenv)
 import os  # import module for directory management
-import datetime
-from discord.ext import tasks
-import re
-from discord.utils import get
-from Embeds import RichEmbed
-from data import coordinate_channel, application_channel, vote_emotes, role_list, hammer_guild, role_ids, \
-    vote_role_id
-from coordinates import *
-from utils import *
-from task import task_list
-from bug import mc_bug, regex_normal
+from data import bug_colour_mappings
 
-# discord token is stored in a .env file in the same directory as the bot
+regex_normal = re.compile("((mc|mcapi|mcce|mcds|mcl|mcpe|realms|sc|web)-[0-9]+)", re.IGNORECASE)
+regex_extended = re.compile("((mc|mcapi|mcce|mcds|mcl|mcpe|realms|sc|web)-[0-9]+ extended)", re.IGNORECASE)
+
 load_dotenv()  # load the .env file containing id's that have to be kept secret for security
-TOKEN = os.getenv("DISCORD_TOKEN")  # get our discord bot token from .env
-
-bot = commands.Bot(command_prefix="/")
-
-latest_new_person = ""
+mojira_username = os.getenv("mojira_username")
+mojira_password = os.getenv("mojira_password")
 
 
-# print a message if the bot is online
-@bot.event
-async def on_ready():
-    print("bot connected")
-    # change status to playing mc
-    global latest_new_person
-    await bot.change_presence(activity=discord.Game("Technical Minecraft on HammerSMP"))
+async def mc_bug(message):
 
+    issues = re.findall(regex_normal, message.content)
+    extended = re.findall(regex_extended, message.content)
 
-@bot.event
-async def on_message(message):
-    # check if the bot is online and not responding to itself
-    if message.author == bot.user:
-        return
-
-    # if a new message is sent in the application forms channel, the bot will automatically add reactions
-    if message.channel.id == application_channel:
-        for e in vote_emotes:
-            await message.add_reaction(bot.get_emoji(e))
-
-    await mc_bug(message)
-    await bot.process_commands(message)  # makes sure other commands will also be processed
-
-
-@bot.event
-async def on_member_join(member):
-    global latest_new_person
-    latest_new_person = member
-
-
-@bot.event
-async def on_member_remove(member):
-    global latest_new_person
-    if latest_new_person == member:
-        response = "Sadly, **{}** left us already.".format(member.name)
-        await bot.get_guild(hammer_guild).system_channel.send(response)
-
-
-# command to test if the bot is running
-@bot.command(name="ping", help="test if the bot is working")
-@commands.has_role("members")
-async def ping(ctx):
-    response = "HammerBot Python is online!"
-    await ctx.send(response)
-
-
-@bot.command(name="testing")
-@commands.has_role("members")
-async def testing(ctx):
-    vote_role = ctx.guild.get_role(vote_role_id)
-    print(vote_role)
-
-
-# command to test if the bot is running
-@bot.command(name="role", help="Give yourself the \"tour giver\" role")
-@commands.has_role("members")
-async def role(ctx, action, *args):
-    if action == "list":
-        await ctx.send(role_list)
-        return
-
-    # check if you have provided a role, if not tell the user to do so
-    if not args:
-        response = "You have not specified a role"
-        await ctx.send(response)
-        return
-
-    # combine the *args tuple into a string role
-    role_arg = " ".join(args)
-
-    # give the tour giver role if the user asks for this
-    if role_arg in role_ids:
-        member = ctx.message.author  # the author of the message, part of the discord.Member class
-        guild_role = bot.get_guild(hammer_guild).get_role(role_ids[role_arg])  # the role needed to add
-
-        if action == "add" and guild_role in member.roles:
-            response = "I'm sorry but you already have this role"
-            await ctx.send(response)
-            return
-        elif action == "remove" and guild_role not in member.roles:
-            response = "I'm sorry but you don't have this role."
-            await ctx.send(response)
-            return
-
-        # if the user doesn't have the right perms, throw an exception
-        try:
-            if action == "add":
-                await member.add_roles(guild_role)
-                response = "You have been successfully given the tour giver role! Congratulations."
-                await ctx.send(response)
-
-            elif action == "remove":
-                await member.remove_roles(guild_role)
-                response = "The role has successfully been removed, congratulations"
-                await ctx.send(response)
-
-        except discord.errors.Forbidden:
-            response = "Missing permissions"
-            await ctx.send(response)
-
-    elif role_arg in get_server_roles(ctx):
-        response = "I'm sorry but i'm afraid you can't add/remove that role to yourself using the bot."
-        await ctx.send(response)
-
-    # if the role is not a role one can add, throw an exception
-    else:
-        response = "I'm sorry but i'm afraid that role doesn't exist"
-        await ctx.send(response)
-
-
-# command to test if the bot is running
-@bot.command(name="stop_lazy", help="command to tell someone to stop lazy")
-@commands.has_role("members")
-async def stop_lazy(ctx, mention="jerk"):
-    await ctx.message.delete()
-    response = "Stop Lazy {}".format(mention)
-    await ctx.send(response)
-    await ctx.send(file=discord.File('stop_lazy.png'))
-
-
-@bot.command(name="CMP", help="command to get the CMP IP in your DM's")
-@commands.has_any_role("CMP access", "members")
-async def CMP(ctx, mention="jerk"):
-    CMP_IP = os.getenv("CMP_IP")
-    response = "Check your DM's"
-    await ctx.author.send(CMP_IP)
-    await ctx.send(response)
-
-
-# command to test if the bot is running
-@bot.command(name="vote", help="command to vote")
-@commands.has_role("members")
-async def vote(ctx, vote_type="", *args):
-    await ctx.message.delete()
-
-    if not args:
-        response = "I'm sorry but you haven't specified anything to vote on."
-        await ctx.send(response, delete_after=5)
-
-    if not vote_type or vote_type not in ("yes_no", "multiple"):
-        response = "I'm sorry but you haven't specified a correct vote type."
-        await ctx.send(response, delete_after=5)
-
-    vote_role = ctx.guild.get_role(vote_role_id)
-
-    if vote_type == "yes_no":
-        string_votes = " ".join(args)
-        embed = discord.Embed(
-            colour=0xe74c3c,
-            title=string_votes,
+    if issues:
+        jira = JIRA(
+            server="https://bugs.mojang.com",
+            basic_auth=(mojira_username, mojira_password),
         )
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed.set_footer(text="Poll created on {}".format(str(datetime.datetime.now())[:-7]))
-        poll_message = await ctx.send(embed=embed)
-        for e in vote_emotes:
-            await poll_message.add_reaction(bot.get_emoji(e))
-        ping = await ctx.send(vote_role.mention)
-        await ping.delete()
 
-    elif vote_type == "multiple":
-        poll, poll_list, introduction = format_conversion(args, "poll")
-        embed = discord.Embed(
-            color=0xe74c3c,
-            title=introduction,
-            description=poll[:-1] + " "
-        )
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        embed.set_footer(text="Poll created on {}".format(str(datetime.datetime.now())[:-7]))
-        poll_message = await ctx.send(embed=embed)
-        for n in range(len(poll_list)):
-            await poll_message.add_reaction(discord_letters[n])
-        ping = await ctx.send(vote_role.mention)
-        await ping.delete()
+        for issueid in issues[:3]:
+            try:
+                issue = jira.issue(issueid[0])
+                status = issue.fields.status
+                embed = discord.Embed(
+                    color=bug_colour_mappings[str(status)],
+                    title="**{}**: {}".format(str.upper(issueid[0]), issue.fields.summary),
+                    url=f"https://bugs.mojang.com/browse/{issueid[0]}")
+                embed.set_author(name=issue.fields.creator,
+                                 icon_url=getattr(issue.fields.reporter.avatarUrls, "48x48"),
+                                 url="https://bugs.mojang.com/secure/ViewProfile.jspa?name={}".format(issue.fields.reporter.name))
+                date_time = issue.fields.created.split("T")
+                embed.set_footer(text="created on {} at {}".format(date_time[0], date_time[1][:-9]))
 
+                if extended:
 
-# /bulletin add project description | {first bulletin} & {second bulletin} & {third bulletin}
-@bot.command(name="bulletin")
-@commands.has_role("members")
-async def bulletin(ctx, action, *args):
-    await ctx.message.delete()
-    """if ctx.channel.id != coordinate_channel:
-        response = "Sorry, wrong channel buddy"
-        await ctx.send(response, delete_after=5)
-        return"""
-    await task_list(ctx=ctx, action=action, args=args, use="bulletin")
+                    description = issue.fields.description
 
+                    if len(description) > 500:
+                        description = description[:500] + "..."
+                    embed.description = description
+                    embed.add_field(name="Status", value=status)
+                    embed.add_field(name="Resolution", value=issue.fields.resolution)
+                    embed.add_field(name="Votes", value=issue.fields.votes)
 
-@bot.command(name="todo")
-@commands.has_role("members")
-async def todo(ctx, action, *args):
-    await ctx.message.delete()
-    await task_list(ctx=ctx, action=action, args=args, use="todo")
+                    versions = issue.fields.versions
+                    if len(versions) > 4:
+                        version_field = "{},\n{}\n...\n{},\n{}".format(versions[0].name, versions[1].name,
+                                                                       versions[-2].name, versions[-1].name)
+                    else:
+                        version_field = ""
+                        for version in versions:
+                            version_field += version.name + ",\n"
+                        version_field = version_field[:-2]
+                    embed.add_field(name="Versions", value=version_field)
+                    labels = issue.fields.labels
+                    label_field = ""
+                    if not labels:
+                        label_field = "None"
+                    else:
+                        for label in labels:
+                            label_field += label + "\n"
+                        label_field = label_field[:-1]
+                    embed.add_field(name="Labels", value=label_field)
+                    categories = issue.fields.customfield_11901
+                    category_field = ""
+                    if not categories:
+                        category_field = "None"
 
+                    else:
+                        for category in categories:
+                            category_field += str(category) + "\n"
 
-@bot.command(name="coords")
-@commands.has_role("members")
-async def coords(ctx, action, *args):
-    await ctx.message.delete()
-    if ctx.channel.id == coordinate_channel:
-        await task_list(ctx=ctx, action=action, args=args, use="bulletin")
+                    embed.add_field(name="Category", value=category_field)
+                    embed.add_field(name="Assignee", value=issue.fields.assignee)
+                    embed.add_field(name="Watchers", value=issue.fields.watches.raw["watchCount"])
+                    priority = issue.fields.customfield_12200
+                    if not priority:
+                        embed.add_field(name="Priority", value="None")
+                    else:
+                        embed.add_field(name="Priority", value=issue.fields.customfield_12200.value)
 
+                    if issue.fields.attachment:
+                        for attach in issue.fields.attachment:
+                            regex = re.compile("(png|jpg|jpeg)")
+                            valid = re.findall(regex, attach.content)
+                            if valid:
+                                embed.set_image(url=attach.content)
+                                await message.channel.send(embed=embed)
+                                return
+                    await message.channel.send(embed=embed)
 
-@bot.command(name="mass_delete")
-@commands.has_role("admin")
-async def mass_delete(ctx, number_of_messages):
-    await ctx.message.delete()
-    if number_of_messages > 200:
-        response = "You want to delete too many messages at once, I'm sorry."
-        await ctx.send(response)
-    channel_history = await ctx.channel.history(limit=int(number_of_messages)).flatten()
-    for message in channel_history:
-        await message.delete()
+                else:
+                    embed.description = "**Status:** {} | **Resolution:** {} | **Votes:** {}".format(status, issue.fields.resolution, issue.fields.votes)
+                    await message.channel.send(embed=embed)
 
-
-@tasks.loop(seconds=5)
-async def forms():
-    pass
-
-forms.start()
-bot.run(TOKEN)
+            except:
+                try:
+                    await message.channel.send(f"{issueid[0]} does not exist")
+                except:
+                    await message.channel.send(f"fuck off {message.author.mention}")
