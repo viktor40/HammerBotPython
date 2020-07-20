e4bd631e497c0e670dd4d69d507b4a27806ee0fb
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-07-20 22:34:23 +0200

    (pep 591) made constants in data.py final

--- data.py
+++ data.py
@@ -2,28 +2,30 @@
 # data.py
 
 import discord
+from typing import Final
 
-coordinate_channel = 690080048375529481
-application_channel = 647293235248496644
-bulletin_board_channel = 724371678393663569
+coordinate_channel: Final[int] = 690080048375529481
+application_channel: Final[int] = 647293235248496644
+bulletin_board_channel: Final[int] = 724371678393663569
+vote_channel_id: Final[int] = 730396648995422288
 
-hammer_guild = 645464470633840651
+hammer_guild: Final[int] = 645464470633840651
 
-upvote_emote = 689945780844494848
-downvote_emote = 689946011854307381
-votent_emote = 701747772461678612
+upvote_emote: Final[int] = 689945780844494848
+downvote_emote: Final[int] = 689946011854307381
+votent_emote: Final[int] = 701747772461678612
 
-vote_emotes = [upvote_emote, downvote_emote, votent_emote]
+vote_emotes: Final[list] = [upvote_emote, downvote_emote, votent_emote]
 
-vote_role_id = 729381591197024379
-tour_giver_id = 702542764893536346
+vote_role_id: Final[int] = 729381591197024379
+tour_giver_id: Final[int] = 702542764893536346
 
-role_ids = {"tour giver": vote_role_id, "voting": tour_giver_id}
-role_list = ["tour giver", "voten'tn't"]
+role_ids: Final[dict] = {"tour giver": vote_role_id, "voting": tour_giver_id}
+role_list: Final[list] = ["tour giver", "voten'tn't"]
 
-hammer_bot_id = 724012480661028907
+hammer_bot_id: Final[int] = 724012480661028907
 
-discord_letters = [
+discord_letters: Final[list] = [
     "\N{REGIONAL INDICATOR SYMBOL LETTER A}",
     "\N{REGIONAL INDICATOR SYMBOL LETTER B}",
     "\N{REGIONAL INDICATOR SYMBOL LETTER C}",
@@ -50,11 +52,11 @@ discord_letters = [
     "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
     "\N{REGIONAL INDICATOR SYMBOL LETTER Y}",
     "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"
-]
+    ]
 
-bug_colour_mappings = {"Open": discord.Color.red(),
-                       "In Progress": discord.Colour.orange(),
-                       "Resolved": discord.Colour.green(),
-                       "Reopened": discord.Colour.blue(),
-                       "Postponed": discord.Colour.purple(),
-                       "Closed": discord.Colour.dark_grey()}
+bug_colour_mappings: Final[dict] = {"Open": discord.Color.red(),
+                                    "In Progress": discord.Colour.orange(),
+                                    "Resolved": discord.Colour.green(),
+                                    "Reopened": discord.Colour.blue(),
+                                    "Postponed": discord.Colour.purple(),
+                                    "Closed": discord.Colour.dark_grey()}
