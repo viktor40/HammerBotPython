2b1721d4d2ecd940514e140fd7b5486a895f03b0
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-07-28 01:14:48 +0200

    added code to iterate over the commands and get help, usage, name and permission data

--- help_command/helping.py
+++ help_command/helping.py
@@ -5,14 +5,23 @@ This will later be used for the custom help command. Currently the built in help
 """
 
 
+# this command will go through all commands, fetch the name, usage and help variable given to the
+# discord.ext.commands.Bot.command decorator and pass the context to checks to see if the sender has permissions
+# to use that command
 def permissions(ctx, bot):
     command_list = {}
+    # iterate over all commands
     for com in bot.commands:
+        # register name, usage and help
         name = com.name
         usage = com.usage
         help_text = com.help
+
+        # try and except in case of MissingRole. MissingRole error means the person doesn't have perms to use that.
         try:
             check = com.checks
+            # check if the command actually has any checks (public commands don't for instance)
+            # if there are no checks it will always be possible to use the command, so it's always true
             if check:
                 permitted = com.checks[0](ctx)
                 command_list[name] = (usage, help_text, permitted)
