d8c77e6d1e4da728f62726c5da197a41f3a85421
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-10-28 13:07:53 +0100

    mini delete command for members to purge a max of 10 messages per command

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -37,6 +37,18 @@ class AdminCommands(commands.Cog):
         else:
             await ctx.channel.purge(limit=number_of_messages)
 
+    # A admin only command to mass delete messages in case of a bad discord discussion.
+    @commands.command(name='mini_delete', help=hd.mass_delete_help, usage=hd.mass_delete_usage)
+    @commands.has_role(data.member_role_id)
+    async def mini_delete(self, ctx, number_of_messages: int):
+        await ctx.message.delete()
+        if number_of_messages > 10:
+            response = "You want to delete too many messages at once, I'm sorry."
+            await ctx.send(response)
+            return
+        else:
+            await ctx.channel.purge(limit=number_of_messages)
+
     # This is a command purely for testing purposes during development.
     @commands.command(name='testing', help=hd.testing_help, usage=hd.testing_usage)
     @commands.check(author_is_admin_or_dev)
